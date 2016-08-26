4cd62c646921e4c2d028be2f9d2020c90ed6d95f
Abumeragan <rodrigo.uba@gmail.com>
2016-08-25 22:05:22 -0300

    Inicio do projeto, criação dos models Aplicativo, Arduino, Rele, TemperaturaUmidade e User

---
+++ Model/Aplicativo.php
@@ -0,0 +1,20 @@
+<?PHP 
+    public class Aplicativo{
+        private $nome;
+        private $MAC;
+                
+        public function __construct($nome, $MAC){
+            $this nome -> $nome;
+            $this mac -> $MAC;
+        }
+        
+        public function getNome(){
+            return $this -> nome;
+        }
+        
+        public function getMAC(){
+            return $this -> mac;
+        }
+        
+    }
+?>
