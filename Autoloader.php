60cb0fa6d1cc80025139afeff72e1bf5258b08fa
LeonardoCalado <leonardosilva.uba@hotmail.com>
2016-09-05 10:14:47 -0300

    corrigindo includes

--- Autoloader.php
+++ Autoloader.php
@@ -1,18 +1,6 @@
-<?php
-
-    function loadClass($classname) {
-            if(is_readable('Class/'.$classname.'.php')) {
-                require 'Class/'.$classname.'.php';
-            }
-        }
-        
-        function loadModels($classname) {
-            if(is_readable('Models/'.$classname.'.php')) {
-                require 'Models/'.$classname.'.php';
-            }
-        }
-        
-        spl_autoload_register('loadClass');
-        spl_autoload_register('loadModels');
-
-?>
+// Importa todas as rotas
+include 'Routes/AplicativoRouter.php';
+//include 'Routes/ArduinoRouter.php';
+//include 'Routes/ReleRouter.php';
+//include 'Routes/TempUmiRouter.php';
+//include 'Routes/UserRouter.php';
