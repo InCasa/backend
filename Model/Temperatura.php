784309ba55915926aebf28fb071186e0406e7c6c
LeonardoCalado <leonardosilva.uba@hotmail.com>
2016-09-15 10:06:55 -0300

    Separação do model TempUmi

---
+++ Model/Temperatura.php
@@ -0,0 +1,25 @@
+<?php 
+    public class sensorTemperatura(){
+        private $nome;
+        private $descricao;
+        private $valor;        
+        
+        public function __construct($nome, $descricao, $valor){
+            $this->nome = $nome;
+            $this->descricao = $descricao;
+            $this->valor = $valor;            
+        }
+        
+        public function getNome(){
+            return $this->nome;
+        }
+        
+        public function getDescricao(){
+            return $this->descricao;
+        }
+        
+        public function getValor(){
+            return $this->valor;
+        }
+                
+    }
