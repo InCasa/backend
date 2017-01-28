5582eb24559ba2fed678af54734acad8029de718
Abumeragan <rodrigo.uba@gmail.com>
2017-01-28 18:16:15 -0200

    Modificado para pegar o valor do Ip do model Arduino

--- class/CurlRele.php
+++ class/CurlRele.php
@@ -1,44 +1,48 @@
 <?php
 
-    class CurlRele {
+    class CurlRele {        
 
         public static function ligaRele($rele) {
+            $arduinoDAO = new ArduinoDAO();
+            $arduino = $arduinoDAO->getArduino(1);
             switch ($rele) {
                 case 1:
-                    $ch = curl_init("http://192.168.1.150/?1_ligar");
+                    $ch = curl_init("http://".$arduino->getIP()."/?1_ligar");
                     curl_exec($ch);
                     break;
                 case 2:
-                    $ch = curl_init("http://192.168.1.150/?2_ligar");
+                    $ch = curl_init("http://".$arduino->getIP()."/?2_ligar");
                     curl_exec($ch);
                     break;
                 case 3:
-                    $ch = curl_init("http://192.168.1.150/?3_ligar");
+                    $ch = curl_init("http://".$arduino->getIP()."/?3_ligar");
                     curl_exec($ch);
                     break;
                 case 4:
-                    $ch = curl_init("http://192.168.1.150/?4_ligar");
+                    $ch = curl_init("http://".$arduino->getIP()."/?4_ligar");
                     curl_exec($ch);
                     break;    
             }
         }
 
         public static function desligaRele($rele) {
+            $arduinoDAO = new ArduinoDAO();
+            $arduino = $arduinoDAO->getArduino(1);
             switch ($rele) {
                 case 1:
-                    $ch = curl_init("http://192.168.1.150/?1_desligar");
+                    $ch = curl_init("http://".$arduino->getIP()."/?1_desligar");
                     curl_exec($ch);
                     break;
                 case 2:
-                    $ch = curl_init("http://192.168.1.150/?2_desligar");
+                    $ch = curl_init("http://".$arduino->getIP()."/?2_desligar");
                     curl_exec($ch);
                     break;
                 case 3:
-                    $ch = curl_init("http://192.168.1.150/?3_desligar");
+                    $ch = curl_init("http://".$arduino->getIP()."/?3_desligar");
                     curl_exec($ch);
                     break;
                 case 4:
-                    $ch = curl_init("http://192.168.1.150/?4_desligar");
+                    $ch = curl_init("http://".$arduino->getIP()."/?4_desligar");
                     curl_exec($ch);
                     break;    
             }
