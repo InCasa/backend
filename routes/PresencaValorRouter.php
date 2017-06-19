61c31cef9596000e1a7f0ebe6b8a7cd67e8982e6
Abumeragan <rodrigo.uba@gmail.com>
2017-06-19 16:12:43 -0300

    Criado método exclusivo para responder o modo Alarme

--- routes/PresencaValorRouter.php
+++ routes/PresencaValorRouter.php
@@ -25,6 +25,32 @@
         $newResponse = $response->withJson($json);
         return $newResponse;
 	})->add($authBasic);
+
+    $app->get('/presencaValorNow',  function ($request, $response) {	
+        //esse metodo vai retornar apenas o valor direto do arduino, so salva caso o valor seja true;		
+        $presencaValorDAO = new PresencaValorDAO();
+
+        $arduinoDAO = new ArduinoDAO();
+        $arduino = $arduinoDAO->getArduino(1);
+
+        //configura a requesição do PHP para o arduino com timeout.
+        $opts = array('http' =>array('method'  => 'GET','timeout' => 1));
+        $context  = stream_context_create($opts);
+        $jsonPresencaValor = file_get_contents("http://".$arduino->getIP()."/?movimento", false, $context);
+        $movimento = json_decode($jsonPresencaValor, true); 
+
+        if($movimento["valor"] == 1){
+            $presencaValor = new PresencaValor();
+            $presencaValor->setValor($movimento["valor"]);
+            $presencaValor->setIdSensorPresenca(1);
+            $presencaValorDAO->create($presencaValor);
+        }       
+		
+        $json = array('valor'=>$movimento["valor"]);
+                
+        $newResponse = $response->withJson($json);
+        return $newResponse;
+	})->add($authBasic);
     
     $app->get('/presencaValor/{id}',  function ($request, $response) {			
         $id = $request->getAttribute('id');
